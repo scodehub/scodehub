4fdf04f1eb206904be6c18338e5d148a4881bc0e
scodehub <contact.praok@gmail.com>
2025-04-20 00:06:04 +0300

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -1,6 +1,7 @@
 local Games = {
   [70876832253163] = "https://raw.githubusercontent.com/gumanba/Scripts/main/DeadRails", -- ScodeHub | Dead Rails
   [2260217507] = "https://raw.githubusercontent.com/scodehub/scodehub/refs/heads/main/weirdthing", -- ScodeHub | Türk Tanışma Oyunu
+  [4483381587] = "https://raw.githubusercontent.com/scodehub/scodehub/refs/heads/main/weirdthing", -- ScodeHub | Baseplate
 }
 
 return Games
