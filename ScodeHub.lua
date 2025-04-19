97cc2b00f2c52fc5b033136c92c399da14c34dfc
scodehub <contact.praok@gmail.com>
2025-04-19 23:34:29 +0300

    Update ScodeHub.lua

--- ScodeHub.lua
+++ ScodeHub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/scodehub/scodehub/refs/heads/main/GameList.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
